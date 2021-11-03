4fce2919a8c37e4be1554883a5788d4e444f0ef5
Joseph Micla <josephmicla@gmail.com>
2021-11-03 20:37:24 +0000

    refactored handleInput and setInput into input.js file

---
+++ input.js
@@ -0,0 +1,21 @@
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  // handles user inputs
+  stdin.on('data', handleUserInput);
+
+  //return the stdin object which allows us to listen to KB input and react to it
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,28 +1,7 @@
 const { connect } = require('./client');
-
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  // handles user inputs
-  stdin.on('data', handleUserInput);
-
-  //return the stdin object which allows us to listen to KB input and react to it
-  return stdin;
-};
-
-const handleUserInput = (key) => {
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-setupInput();
-
-
+const { setupInput } = require('./input');
 
 console.log('Connecting...');
 connect();
+
+setupInput();
