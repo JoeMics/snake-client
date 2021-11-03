4a4aa25e323f7756fb6923b5ef3c81c07f2f9321
Joseph Micla <josephmicla@gmail.com>
2021-11-03 19:54:33 +0000

    created code to connect to snake server

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+// establishes a connection with the game server
+const connect = () => {
+  const conn = net.createConnection({
+    host: '165.227.47.243', // IP
+    port: 50541             // PORT
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  // logs data comming from the server when it sends it
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+console.log('Connecting...');
+connect();
